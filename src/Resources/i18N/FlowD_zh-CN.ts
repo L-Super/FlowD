6a79ce059ca25015fad2c03b5cdf346bab723f65
L-Super <120311070@qq.com>
2024-09-03 14:27:33 +0800

    feat: add translations in cmake

---
+++ src/Resources/i18N/FlowD_zh-CN.ts
@@ -0,0 +1,391 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>DownloadItemWidget</name>
+    <message>
+        <location filename="../../DownloadItemWidget.ui" line="46"/>
+        <source>//  File Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.ui" line="86"/>
+        <location filename="../../DownloadItemWidget.cpp" line="151"/>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.ui" line="111"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.ui" line="136"/>
+        <source>File Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.ui" line="155"/>
+        <source>Remaining time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.ui" line="174"/>
+        <source>Speed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="49"/>
+        <location filename="../../DownloadItemWidget.cpp" line="164"/>
+        <source>File Size: %1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="65"/>
+        <source>Speed: %1 %2/s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="147"/>
+        <source>Continue</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DownloadListWidget</name>
+    <message>
+        <location filename="../../DownloadListWidget.ui" line="14"/>
+        <source>Download List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadListWidget.ui" line="42"/>
+        <source>Downloading</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadListWidget.ui" line="66"/>
+        <source>Stoppped</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpWidget</name>
+    <message>
+        <location filename="../../HelpWidget.ui" line="14"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../MainWindow.ui" line="20"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="94"/>
+        <source>     New Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="137"/>
+        <source>     Download List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="193"/>
+        <source>       Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../MainWindow.ui" line="236"/>
+        <source>       Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewDownloadDialog</name>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="20"/>
+        <source>Create a Download Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="51"/>
+        <source>URL Task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="98"/>
+        <source>One task URL per line:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="150"/>
+        <source>Rename (Optional, corresponding to the download links above):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="204"/>
+        <source>Splits:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="267"/>
+        <source>Save Path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="296"/>
+        <source>Select...(&amp;S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="332"/>
+        <source>User-Agent：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="393"/>
+        <source>Authentication：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="454"/>
+        <source>Referer：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="515"/>
+        <source>Cookie：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="586"/>
+        <source>Jump to Download Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="605"/>
+        <source>Advanced Options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="637"/>
+        <source>OK(&amp;O)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../NewDownloadDialog.ui" line="656"/>
+        <source>Cancel(&amp;C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="102"/>
+        <source>%1 Day %2 Hours %3 Minutes %4 Seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="104"/>
+        <source>%1 Hours %2 Minutes %3 Seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="106"/>
+        <source>%1 Minutes %2 Seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../DownloadItemWidget.cpp" line="108"/>
+        <source>%1 Seconds</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsAdvancedWidget</name>
+    <message>
+        <location filename="../../SettingsAdvancedWidget.ui" line="14"/>
+        <source>Advanced Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsAdvancedWidget.ui" line="39"/>
+        <source>User-Agent:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsAdvancedWidget.ui" line="58"/>
+        <source>Config User-Agent</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsBasicWidget</name>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="14"/>
+        <source>Basic Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="77"/>
+        <source>Appereance:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="108"/>
+        <source>System Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="113"/>
+        <source>Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="118"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="147"/>
+        <source>Language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="166"/>
+        <source>System Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="171"/>
+        <source>简体中文</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="176"/>
+        <source>繁体中文</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="181"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="212"/>
+        <source>Startup:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="256"/>
+        <source>Open at login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="280"/>
+        <source>Display main interface after automatic startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="304"/>
+        <source>Automatically resume all unfinished tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="334"/>
+        <source>Default Path:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="368"/>
+        <source>Select(&amp;S)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="398"/>
+        <source>Task Management:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="444"/>
+        <source>Download Splits:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="492"/>
+        <source>Automatically jump to the download page after creating a new task</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="516"/>
+        <source>Notify after download completion</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="540"/>
+        <source>No confirmation required before deleting tasks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="570"/>
+        <source>Automatic upgrade:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.ui" line="594"/>
+        <source>Enable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsBasicWidget.cpp" line="93"/>
+        <source>Select a folder for file saving</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <location filename="../../SettingsWidget.ui" line="14"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsWidget.ui" line="42"/>
+        <source>Basic Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../SettingsWidget.ui" line="66"/>
+        <source>Advanced Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UserAgentConfigDialog</name>
+    <message>
+        <location filename="../../UserAgentConfigDialog.ui" line="14"/>
+        <source>Config User-Agent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UserAgentConfigDialog.ui" line="43"/>
+        <source>User-Agent：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UserAgentConfigDialog.ui" line="125"/>
+        <source>OK(&amp;O)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UserAgentConfigDialog.ui" line="144"/>
+        <source>Cancel(&amp;C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
