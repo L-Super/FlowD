d275abce6bec35bb2cc85cadcc84005df4b072aa
L-Super <120311070@qq.com>
2024-09-22 10:29:28 +0800

    feat: get save file path and thread count from dialog

--- src/NewDownloadDialog.cpp
+++ src/NewDownloadDialog.cpp
@@ -1,8 +1,8 @@
 #include "NewDownloadDialog.h"
 #include "ui_NewDownloadDialog.h"
 
-#include <QDebug>
 #include <QStandardPaths>
+#include <thread>
 
 #include "Logger.hpp"
 
@@ -10,6 +10,10 @@ NewDownloadDialog::NewDownloadDialog(QWidget* parent) : QDialog(parent), ui(new 
 {
     ui->setupUi(this);
     ui->savePathLineEdit->setPlaceholderText(QStandardPaths::writableLocation(QStandardPaths::DownloadLocation));
+    unsigned int cpuCoreCount = std::thread::hardware_concurrency();
+    ui->spinThreadCount->setMinimum(1);
+    ui->spinThreadCount->setMaximum(static_cast<int>(cpuCoreCount) * 2);
+    ui->spinThreadCount->setValue(static_cast<int>(cpuCoreCount));
     showAdvanceOption(false);
     connect(ui->advancedCheckBox, &QCheckBox::clicked, this, &NewDownloadDialog::showAdvanceOption);
     connect(ui->okButton, &QPushButton::clicked, this, [this] {
@@ -42,4 +46,14 @@ QString NewDownloadDialog::downloadUrl()
         return {};
     //TODO:return clean url and check valid
     return ui->urlTextEdit->toPlainText();
+}
+
+QString NewDownloadDialog::saveFilePath()
+{
+    return ui->savePathLineEdit->text();
+}
+
+unsigned int NewDownloadDialog::threadCount()
+{
+    return ui->spinThreadCount->value();
 }
